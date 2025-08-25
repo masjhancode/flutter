d4012379e9d1e40f7c9cc84578ff3a057ef5c182
Shafiyya Mahira Ulinnuha <via@Shafiyyas-MacBook-Air.local>
2025-08-25 13:01:37 +0900

    update main file

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,9 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text(
+              'tekan tombol butten untuk melihat perubahan increment counter',
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
